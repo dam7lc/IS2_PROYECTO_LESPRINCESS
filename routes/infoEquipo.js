13fbf0f3454d02633671124bce1c4488f228e35e
PMan <dam7lc@hotmail.com>
2022-05-28 19:50:39 -0500

    Agregada ruta y view de la seccion 1 informacion del equipo

--- routes/infoEquipo.js
+++ routes/infoEquipo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('autor');
+  res.render('infoEquipo');
 });
 
 module.exports = router;
